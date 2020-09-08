98241349314a78624dfd3d1239adfe7bb1bee585
Tim Boudreaux <tmboudreaux00@gmail.com>
2020-09-08 14:47:21 -0500

    limit exercises

--- db/where_exercises.sql
+++ db/where_exercises.sql
@@ -8,6 +8,7 @@ USE employees;
 
 SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya');
 SELECT * FROM employees WHERE first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya';
+SELECT * FROM employees WHERE (first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya') AND gender = 'M';
 SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya') AND gender = 'M';
 
 SELECT * FROM employees WHERE last_name LIKE 'E%';
